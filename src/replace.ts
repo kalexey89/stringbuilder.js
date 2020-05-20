434f006f4f351cd9e8694e4f8b87a28ae0fb0a07
kalexey <a.kuznetsov@draewil.com>
2020-05-20 13:52:41 +0300

    Fix replace methods

--- src/replace.ts
+++ src/replace.ts
@@ -2,7 +2,7 @@ import { insert } from './insert';
 import { remove } from './remove';
 
 export function replaceRange(start: number, end: number, value: any, str: string): string {
-  return insert(value, remove(start, end, str));
+  return insert(start, value, remove(start, end, str));
 }
 
 export function replacePattern(pattern: string, value: any, str: string): string;
@@ -20,7 +20,7 @@ export function replaceFirst(pattern: string, value: any, ignoreCase: boolean, s
     ? str.toLocaleLowerCase().indexOf(pattern.toLocaleLowerCase())
     : str.indexOf(pattern);
 
-  return index >= 0 ? replaceRange(index, index + String(value).length, value, str) : str;
+  return index >= 0 ? replaceRange(index, index + String(pattern).length, value, str) : str;
 }
 
 export function replaceLast(pattern: string, value: any, ignoreCase: boolean, str: string): string {
@@ -28,5 +28,5 @@ export function replaceLast(pattern: string, value: any, ignoreCase: boolean, st
     ? str.toLocaleLowerCase().lastIndexOf(pattern.toLocaleLowerCase())
     : str.lastIndexOf(pattern);
 
-  return index >= 0 ? replaceRange(index, index + String(value).length, value, str) : str;
+  return index >= 0 ? replaceRange(index, index + String(pattern).length, value, str) : str;
 }
